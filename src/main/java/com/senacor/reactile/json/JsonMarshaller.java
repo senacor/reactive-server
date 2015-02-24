69aa69d8dc770e027f76e403d89c9e7931051252
Michael Omann <michael.omann@gmail.com>
2015-02-24 17:41:33 +0100

    son marshaller for response objects

---
+++ src/main/java/com/senacor/reactile/json/JsonMarshaller.java
@@ -0,0 +1,37 @@
+package com.senacor.reactile.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+public class JsonMarshaller {
+    private final static ObjectMapper om = new ObjectMapper();
+
+    {
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        om.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        om.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        om.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+        om.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+        om.configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false);
+        //allow objects without properties
+        om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    }
+
+    public byte[] toJson(Object object) {
+        try {
+            return om.writeValueAsBytes(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String toJsonString(Object object) {
+        try {
+            return om.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
