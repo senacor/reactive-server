feab69f45cf435bc1d20a051430c9e1e9d7cee84
Ralph Winzinger <ralph.winzinger@senacor.com>
2015-02-26 10:02:59 +0100

    timeout to wait for mongo

--- src/test/java/com/senacor/reactile/customer/CustomerServiceVerticleTest.java
+++ src/test/java/com/senacor/reactile/customer/CustomerServiceVerticleTest.java
@@ -2,6 +2,7 @@ package com.senacor.reactile.customer;
 
 import com.senacor.reactile.EventBusRule;
 import com.senacor.reactile.VertxRule;
+import com.senacor.reactile.bootstrap.ApplicationStartup;
 import io.vertx.rxjava.core.eventbus.Message;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,14 +17,16 @@ import static org.junit.Assert.assertThat;
 public class CustomerServiceVerticleTest {
 
     @Rule
-    public final VertxRule vertxRule = new VertxRule(CustomerServiceVerticle.class);
+    public final VertxRule vertxRule = new VertxRule(ApplicationStartup.class, CustomerServiceVerticle.class);
 
     @Rule
     public final EventBusRule eventBusRule = new EventBusRule(vertxRule.vertx());
 
     @Test
     public void thatVerticleRespondsToMessage() throws InterruptedException, ExecutionException, TimeoutException {
-        CustomerId customerId = new CustomerId("007");
+        Thread.sleep(5000);
+
+        CustomerId customerId = new CustomerId("08-cust-15");
         Message<Customer> customer = eventBusRule.sendObservable(CustomerServiceVerticle.ADDRESS, customerId);
         assertThat(customer.body().getId(), is(equalTo(customerId)));
     }
