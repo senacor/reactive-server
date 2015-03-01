1ef8f3853ec8f48bb87358abcc041dcaa55fd6a7
Michael Omann <michael.omann@gmail.com>
2015-03-02 00:30:27 +0100

    extract initialiser logic for reuse

---
+++ src/test/java/com/senacor/reactile/customer/CustomerMongoInitializer.java
@@ -0,0 +1,84 @@
+package com.senacor.reactile.customer;
+
+import io.vertx.core.Vertx;
+import io.vertx.ext.mongo.MongoService;
+import io.vertx.ext.mongo.WriteOption;
+import io.vertx.rx.java.ObservableFuture;
+import io.vertx.rx.java.RxHelper;
+import rx.Observable;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CustomerMongoInitializer {
+
+    private final io.vertx.rxjava.core.Vertx vertx;
+
+    public CustomerMongoInitializer(io.vertx.rxjava.core.Vertx vertx) {
+        this.vertx = vertx;
+    }
+
+    public void write(int count) {
+        MongoService service = MongoService.createEventBusProxy((Vertx) vertx.getDelegate(), "vertx.mongo");
+
+        Observable<Customer> testCustomers = Observable.zip(
+                addressNumber(count),
+                firstName(),
+                lastName(),
+                streetName(),
+                streetType(), (addrNum, fname, lname, sname, stype) -> {
+                    Address addr = Address.anAddress()
+                            .withAddressNumber("addr-" + addrNum)
+                            .withStreet(sname + stype)
+                            .withCoHint("")
+                            .withCity("Nürnberg")
+                            .withZipCode("12345")
+                            .withCountry(new Country("Deutschland", "DE")).build();
+
+                    return Customer.newBuilder()
+                            .withId("cust-" + addrNum)
+                            .withFirstname(fname)
+                            .withLastname(lname)
+                            .withAddresses(Arrays.asList(addr))
+                            .withTaxNumber("tax-" + addrNum)
+                            .withTaxCountry(new Country("Deutschland", "DE"))
+                            .build();
+                }
+        );
+
+        testCustomers.flatMap(customer -> {
+            ObservableFuture<String> newOne = RxHelper.observableFuture();
+            service.insertWithOptions("customers", customer.toJson(), WriteOption.UNACKNOWLEDGED, newOne.asHandler());
+            return newOne;
+        }).subscribe(outcome -> {
+            System.out.println("outcome = " + outcome);
+        }, Throwable::printStackTrace, () -> {
+            System.out.println("done!!!!");
+        });
+    }
+
+
+    private Observable<Integer> addressNumber(int count) {
+        return Observable.range(1000, count);
+    }
+
+    private Observable<String> streetName() {
+        List streets = Arrays.asList("Winter", "Frühling", "Sommer", "Herbst", "Amsel", "Drossel", "Fink", "Star");
+        return Observable.from(streets).repeat();
+    }
+
+    private Observable<String> streetType() {
+        List streets = Arrays.asList("strasse", "weg", "pfad");
+        return Observable.from(streets).repeat();
+    }
+
+    private Observable<String> firstName() {
+        List streets = Arrays.asList("Adam", "Anneliese", "Berthold", "Berta", "Christopher", "Charlotte", "Dennis", "Dorothea");
+        return Observable.from(streets).repeat();
+    }
+
+    private Observable<String> lastName() {
+        List streets = Arrays.asList("Kugler", "Lurchig", "Monheim", "Naaber", "Peine", "Quaid", "Rastatt");
+        return Observable.from(streets).repeat();
+    }
+}
--- src/test/java/com/senacor/reactile/customer/CustomerServiceVerticleTest.java
+++ src/test/java/com/senacor/reactile/customer/CustomerServiceVerticleTest.java
@@ -4,18 +4,10 @@ import com.senacor.reactile.EventBusRule;
 import com.senacor.reactile.Services;
 import com.senacor.reactile.VertxRule;
 import com.senacor.reactile.bootstrap.ApplicationStartup;
-import io.vertx.core.Vertx;
-import io.vertx.ext.mongo.MongoService;
-import io.vertx.ext.mongo.WriteOption;
-import io.vertx.rx.java.ObservableFuture;
-import io.vertx.rx.java.RxHelper;
 import io.vertx.rxjava.core.eventbus.Message;
 import org.junit.Rule;
 import org.junit.Test;
-import rx.Observable;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -34,6 +26,8 @@ public class CustomerServiceVerticleTest {
     @Rule
     public final EventBusRule eventBusRule = new EventBusRule(vertxRule.vertx());
 
+    private final CustomerMongoInitializer initializer = new CustomerMongoInitializer(vertxRule.vertx());
+
     @Test
     public void thatVerticleRespondsToMessage() throws InterruptedException, ExecutionException, TimeoutException {
         CustomerId customerId = new CustomerId("08-cust-15");
@@ -43,70 +37,9 @@ public class CustomerServiceVerticleTest {
 
     @Test
     public void fillWithTestdata() {
-        MongoService service = MongoService.createEventBusProxy((Vertx) vertxRule.vertx().getDelegate(), "vertx.mongo");
+        initializer.write(1000);
 
-        Observable<Customer> testCustomers = Observable.zip(
-                addressNumber(),
-                firstName(),
-                lastName(),
-                streetName(),
-                streetType(), (addrNum, fname, lname, sname, stype) -> {
-                    Address addr = Address.anAddress()
-                            .withAddressNumber("addr-"+addrNum)
-                            .withStreet(sname+stype)
-                            .withCoHint("")
-                            .withCity("Nürnberg")
-                            .withZipCode("12345")
-                            .withCountry(new Country("Deutschland", "DE")).build();
-
-                    return Customer.newBuilder()
-                            .withId("cust-"+addrNum)
-                            .withFirstname(fname)
-                            .withLastname(lname)
-                            .withAddresses(Arrays.asList(addr))
-                            .withTaxNumber("tax-"+addrNum)
-                            .withTaxCountry(new Country("Deutschland", "DE"))
-                            .build();
-                }
-        );
-
-
-
-
-        testCustomers.flatMap(customer -> {
-            ObservableFuture<String> newOne = RxHelper.observableFuture();
-            service.insertWithOptions("customers", customer.toJson(), WriteOption.UNACKNOWLEDGED, newOne.asHandler());
-            return newOne;
-        }).subscribe(outcome -> {
-            System.out.println("outcome = " + outcome);
-        }, Throwable::printStackTrace,()-> {
-            System.out.println("done!!!!");
-        });
     }
 
-
-    private Observable<Integer> addressNumber() {
-        return Observable.range(1000, 100000);
-    }
-
-    private Observable<String> streetName() {
-        List streets = Arrays.asList("Winter", "Frühling", "Sommer", "Herbst", "Amsel", "Drossel", "Fink", "Star");
-        return Observable.from(streets).repeat();
-    }
-
-    private Observable<String> streetType() {
-        List streets = Arrays.asList("strasse", "weg", "pfad");
-        return Observable.from(streets).repeat();
-    }
-
-    private Observable<String> firstName() {
-        List streets = Arrays.asList("Adam", "Anneliese", "Berthold", "Berta", "Christopher", "Charlotte", "Dennis", "Dorothea");
-        return Observable.from(streets).repeat();
-    }
-
-    private Observable<String> lastName() {
-        List streets = Arrays.asList("Kugler", "Lurchig", "Monheim", "Naaber", "Peine", "Quaid", "Rastatt");
-        return Observable.from(streets).repeat();
-    }
 
 }
