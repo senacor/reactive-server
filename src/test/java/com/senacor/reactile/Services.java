5ba4fc8496e383b4e15505a34146db6ca1b384da
Ralph Winzinger <ralph.winzinger@senacor.com>
2015-03-03 13:05:04 +0100

    new services

--- src/test/java/com/senacor/reactile/Services.java
+++ src/test/java/com/senacor/reactile/Services.java
@@ -6,6 +6,8 @@ public enum Services implements ServiceIdProvider {
     UserService("com.senacor:reactile-user-service:1.0.0"),
     CustomerService("com.senacor:reactile-customer-service:1.0.0"),
     AccountService("com.senacor:reactile-account-service:1.0.0"),
+    CreditCardService("com.senacor:reactile-creditcard-service:1.0.0"),
+    TransactionService("com.senacor:reactile-transaction-service:1.0.0"),
     MongoConnector("io.vertx:vertx-mongo-embedded-db");
 
     private final String serviceName;
