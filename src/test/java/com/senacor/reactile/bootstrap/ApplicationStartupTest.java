7149880f119187375835b7cd31b4ff194f5066a8
Michael Omann <michael.omann@gmail.com>
2015-03-09 15:37:15 +0100

    fix test

--- src/test/java/com/senacor/reactile/bootstrap/ApplicationStartupTest.java
+++ src/test/java/com/senacor/reactile/bootstrap/ApplicationStartupTest.java
@@ -7,10 +7,6 @@ import org.junit.Test;
 
 import java.util.Set;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 /**
  * Created by rwinzing on 24.02.15.
  */
@@ -19,15 +15,14 @@ public class ApplicationStartupTest {
     public final VertxRule vertxRule = new VertxRule(ApplicationStartup.class);
     private final Vertx vertx = vertxRule.vertx();
 
-    @Test
+    @Test(timeout = 5000)
     public void thatAllNecessaryVerticlesLaunched() throws InterruptedException {
-        Thread.sleep(5000);
-
         Set<String> deployments = vertx.deploymentIDs();
-        for (String deployment: deployments) {
-            System.out.println("deployment = " + deployment);
+        deployments.forEach(deployment -> System.out.println("deployment = " + deployment));
+
+        while(deployments.size() == 3){
+            Thread.sleep(100);
         }
 
-        assertThat(deployments.size(), is(equalTo(3)));
     }
 }
