09d47e4f54be99cd3e8b3b777bc2fc8e15c602a6
Michael Omann <michael.omann@gmail.com>
2015-03-09 14:42:15 +0100

    remove test, externalize string

--- src/test/java/com/senacor/reactile/customer/CustomerFromToJsonTest.java
+++ src/test/java/com/senacor/reactile/customer/CustomerFromToJsonTest.java
@@ -1,10 +1,14 @@
 package com.senacor.reactile.customer;
 
+import com.google.common.io.Resources;
 import io.vertx.core.json.JsonObject;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -14,16 +18,9 @@ import static org.junit.Assert.assertThat;
  * Created by rwinzing on 25.02.15.
  */
 public class CustomerFromToJsonTest {
-    @Test
-    public void thatValidJsonIsGenerated() {
-        Customer customer = getSampleCustomer();
-
-        JsonObject ref = new JsonObject(getSampleCustomerJson());
-        assertThat(customer.toJson(), is(equalTo(ref)));
-    }
 
     @Test
-    public void thatJsonCanBeParsed() {
+    public void thatJsonCanBeParsed() throws IOException {
         String json = getSampleCustomerJson();
 
         Customer customer = Customer.fromJson(new JsonObject(json));
@@ -32,7 +29,7 @@ public class CustomerFromToJsonTest {
         assertThat(customer, is(equalTo(refCustomer)));
     }
 
-    private Customer getSampleCustomer() {
+    private static Customer getSampleCustomer() {
         List<Address> addresses = new ArrayList<>();
         addresses.add(Address.anAddress()
                         .withAddressNumber("1")
@@ -52,25 +49,13 @@ public class CustomerFromToJsonTest {
         return customer;
     }
 
-    private String getSampleCustomerJson() {
-        String json = "{\n" +
-                "  \"taxCountry\" : {\n" +
-                "    \"name\" : \"England\",\n" +
-                "    \"code\" : \"EN\"\n" +
-                "  },\n" +
-                "  \"addresses\" : [ {\n" +
-                "    \"addressNumber\" : \"1\",\n" +
-                "    \"zipCode\" : \"12345\",\n" +
-                "    \"coHint\" : \"c/o Mustermann\",\n" +
-                "    \"city\" : \"Sommerdorf\",\n" +
-                "    \"street\" : \"Winterstrasse\"\n" +
-                "  } ],\n" +
-                "  \"_id\" : \"54eed97c58e770b72068ebf7\",\n" +
-                "  \"id\" : \"08-cust-15\",\n" +
-                "  \"taxnumber\" : \"47-tax-11\",\n" +
-                "  \"contacts\" : [ ]\n" +
-                "}";
+    private String getSampleCustomerJson() throws IOException {
+        return convertStreamToString(Resources.getResource("customer.json").openStream());
+    }
 
-        return json;
+    private static String convertStreamToString(InputStream is) {
+        try(Scanner s = new java.util.Scanner(is).useDelimiter("\\A")) {
+            return s.next();
+        }
     }
 }
