db06b393d0e28d9777d031f6ddf55a74a714cc81
Michael Omann <michael.omann@gmail.com>
2015-03-10 11:18:09 +0100

    add interface as extension to MongoService

---
+++ src/main/java/com/senacor/reactile/mongo/ObservableMongoService.java
@@ -0,0 +1,29 @@
+package com.senacor.reactile.mongo;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoService;
+import io.vertx.rx.java.ObservableFuture;
+
+import java.util.List;
+
+public interface ObservableMongoService extends MongoService {
+
+    ObservableFuture<String> save(String collection, JsonObject document);
+
+    ObservableFuture<String> insert(String collection, JsonObject document);
+
+    ObservableFuture<List<JsonObject>> find(String collection, JsonObject query);
+
+    ObservableFuture<JsonObject> findOne(String collection, JsonObject query);
+
+    ObservableFuture<JsonObject> findOne(String collection, JsonObject query, JsonObject fields);
+
+    ObservableFuture<Long> count(String collection, JsonObject query);
+
+    ObservableFuture<Void> remove(String collection, JsonObject query);
+
+    ObservableFuture<Void> dropCollection(String collection);
+
+    ObservableFuture<JsonObject> runCommand(JsonObject command);
+
+}
