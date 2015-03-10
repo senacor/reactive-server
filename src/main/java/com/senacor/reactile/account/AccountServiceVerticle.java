13696a65888cde45a75624504002e90673a708da
Michael Omann <michael.omann@gmail.com>
2015-03-10 23:13:30 +0100

    externalize mongo collection, apply new ObservableMongoService

--- src/main/java/com/senacor/reactile/account/AccountServiceVerticle.java
+++ src/main/java/com/senacor/reactile/account/AccountServiceVerticle.java
@@ -1,16 +1,18 @@
 package com.senacor.reactile.account;
 
 import com.senacor.reactile.customer.CustomerId;
+import com.senacor.reactile.mongo.ObservableMongoService;
 import com.senacor.reactile.service.AbstractServiceVerticle;
 import com.senacor.reactile.service.Action;
+import io.vertx.core.Context;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoService;
-import io.vertx.rx.java.ObservableFuture;
-import io.vertx.rx.java.RxHelper;
 import rx.Observable;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by rwinzing on 03.03.15.
@@ -18,36 +20,33 @@ import java.util.List;
 public class AccountServiceVerticle extends AbstractServiceVerticle implements AccountService {
     public static final String ADDRESS = "AccountServiceVerticle";
 
-    private MongoService mongoService;
+    private ObservableMongoService mongoService;
+    private String collection;
+
+    @Override
+    public void init(Vertx vertx, Context context) {
+        super.init(vertx, context);
+        collection = context.config().getString("collection");
+    }
 
     @Override
     public void start() throws Exception {
+        MongoService eventBusProxy = MongoService.createEventBusProxy(getVertx(), "vertx.mongo");
         super.start();
-        mongoService = MongoService.createEventBusProxy(getVertx(), "vertx.mongo");
+        //TODO configuration and guice injection
+        mongoService = ObservableMongoService.from(eventBusProxy);
     }
 
     @Action
     public Observable<List<Account>> getAccountsForCustomer(CustomerId id) {
-        ObservableFuture<List<JsonObject>> observable = RxHelper.observableFuture();
-
         JsonObject query = new JsonObject().put("customerId", id.toValue());
-        mongoService.find("accounts", query, observable.toHandler());
-
-        return observable.map(list -> {
-            List<Account> accs = new ArrayList<Account>();
-            list.forEach(item -> accs.add(Account.fromJson(item)));
-            return accs;
-        });
-        // return observable.flatMap(Observable::from).map(Account::fromJson);
+        return mongoService.find(collection, query)
+                .map(list -> list.stream().map(Account::fromJson).collect(toList()));
     }
 
-    @Action
+    @Action("get")
     public Observable<Account> getAccount(AccountId id) {
-        ObservableFuture<JsonObject> observable = RxHelper.observableFuture();
-
         JsonObject query = new JsonObject().put("id", id.toValue());
-        mongoService.findOne("accounts", query, null, observable.toHandler());
-
-        return observable.map(Account::fromJson);
+        return mongoService.findOne("accounts", query).map(Account::fromJson);
     }
 }
