01f98dcc1cf6a00a2049c6d4707fb3e96a0643ff
Michael Omann <michael.omann@gmail.com>
2015-03-11 10:28:25 +0100

    drop dead code

--- src/main/java/com/senacor/reactile/bootstrap/MongoBootstrap.java
+++ src/main/java/com/senacor/reactile/bootstrap/MongoBootstrap.java
@@ -1,13 +1,8 @@
 package com.senacor.reactile.bootstrap;
 
-import com.senacor.reactile.account.Account;
-import com.senacor.reactile.account.AccountFixtures;
-import com.senacor.reactile.customer.Customer;
-import com.senacor.reactile.customer.CustomerFixtures;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.mongo.MongoService;
 import io.vertx.rx.java.ObservableFuture;
 import io.vertx.rx.java.RxHelper;
 import io.vertx.rxjava.core.AbstractVerticle;
@@ -33,27 +28,4 @@ public class MongoBootstrap extends AbstractVerticle {
         getVertx().deployVerticle("service:io.vertx:mongo-service", new DeploymentOptions().setConfig(config), observableHandler.toHandler());
         return observableHandler;
     }
-
-    private Observable<String> writeSomethingObservable() {
-        MongoService service = MongoService.createEventBusProxy(getVertx(), "vertx.mongo");
-
-        Customer customer = CustomerFixtures.defaultCustomer();
-
-
-        ObservableFuture<String> custObservable = RxHelper.observableFuture();
-        service.insert("customers", customer.toJson(), custObservable.toHandler());
-
-        Account account_1 = AccountFixtures.newAccount1();
-
-        ObservableFuture<String> acc1observable = RxHelper.observableFuture();
-        service.insert("accounts", account_1.toJson(), acc1observable.toHandler());
-
-        Account account_2 = AccountFixtures.newAccount2();
-        ObservableFuture<String> acc2observable = RxHelper.observableFuture();
-        service.insert("accounts", account_2.toJson(), acc2observable.toHandler());
-
-        return custObservable.concatWith(acc1observable).concatWith(acc2observable);
-    }
-
-
 }
