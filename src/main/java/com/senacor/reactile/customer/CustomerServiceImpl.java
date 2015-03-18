709166cf02267d91d8ca8edb8deff27ac8c62066
Michael Omann <michael.omann@gmail.com>
2015-03-18 12:23:13 +0100

    make CustomerService a ServiceProxy

--- src/main/java/com/senacor/reactile/customer/CustomerServiceImpl.java
+++ src/main/java/com/senacor/reactile/customer/CustomerServiceImpl.java
@@ -1,34 +1,56 @@
 package com.senacor.reactile.customer;
 
-import io.vertx.rxjava.core.Vertx;
-import io.vertx.rxjava.core.eventbus.Message;
-import rx.Observable;
+import com.senacor.reactile.mongo.ObservableMongoService;
+import com.senacor.reactile.user.UserId;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonObject;
 
 import javax.inject.Inject;
 
-import static com.senacor.reactile.header.Headers.action;
+import static com.senacor.reactile.customer.Address.anAddress;
+import static io.vertx.core.Future.failedFuture;
+import static io.vertx.core.Future.succeededFuture;
+
 
 public class CustomerServiceImpl implements CustomerService {
-    private final Vertx vertx;
+
+    public static final String COLLECTION = "customers";
+    private final ObservableMongoService mongoService;
 
     @Inject
-    public CustomerServiceImpl(Vertx vertx) {
-        this.vertx = vertx;
+    public CustomerServiceImpl(ObservableMongoService mongoService) {
+        this.mongoService = mongoService;
     }
 
     @Override
-    public Observable<Customer> getCustomer(CustomerId customerId) {
-        return vertx.eventBus()
-                .<Customer>sendObservable(CustomerServiceVerticle.ADDRESS, new CustomerId(customerId.getId()), action("getCustomer"))
-                .map(Message::body);
+    public void getCustomer(CustomerId customerId, Handler<AsyncResult<Customer>> resultHandler) {
+        mongoService.findOne(COLLECTION, customerId.toJson(), null, result -> {
+            if (result.failed()) {
+                resultHandler.handle(failedFuture(result.cause()));
+            } else
+                resultHandler.handle(succeededFuture(Customer.fromJson(result.result())));
+        });
     }
 
     @Override
-    public Observable<Customer> createCustomer(Customer customer) {
-        return vertx.eventBus()
-                .<Customer>sendObservable(CustomerServiceVerticle.ADDRESS, customer, action("add"))
-                .map(Message::body);
+    public void createCustomer(Customer customer, Handler<AsyncResult<Customer>> resultHandler) {
+        JsonObject cust = customer.toJson().put("_id", customer.getId().toValue());
+        mongoService.insert(COLLECTION, customer.toJson(), result -> {
+            if (result.failed()) {
+                resultHandler.handle(failedFuture(result.cause().getMessage()));
+            } else
+                resultHandler.handle(succeededFuture(customer));
+        });
     }
 
+
+    private CustomerAddressChangedEvt newCustomerChangedEvent() {
+        return new CustomerAddressChangedEvt(new UserId("momann"), new CustomerId("007"), anAddress()
+                .withStreet("Erika-Mann-Straße")
+                .withAddressNumber("55")
+                .withCountry(new Country("DE", "Deutschland"))
+                .build());
+    }
 
 }
