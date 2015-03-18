6b462fcc4fa40750369aef87d5297d71b97873f1
Michael Omann <michael.omann@gmail.com>
2015-03-18 09:18:38 +0100

    fix matchers

--- src/test/java/com/senacor/reactile/domain/JsonObjectMatchers.java
+++ src/test/java/com/senacor/reactile/domain/JsonObjectMatchers.java
@@ -6,31 +6,39 @@ import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.equalTo;
 
 public class JsonObjectMatchers {
 
 
     public static Matcher<JsonObject> hasValue(String property, Object value) {
-        return new FeatureMatcher<JsonObject, Object>(equalTo(value), " a json object with property " + property + " and value", "value") {
+        FeatureMatcher<JsonObject, Object> valueMatcher = new FeatureMatcher<JsonObject, Object>(equalTo(value), " a json object with property " + property + " and value", "value") {
             @Override
             protected Object featureValueOf(JsonObject actual) {
                 return actual.getValue(property);
             }
         };
+        return allOf(hasProperty(property), valueMatcher);
     }
 
     public static Matcher<JsonObject> hasProperty(String property) {
         return new TypeSafeDiagnosingMatcher<JsonObject>() {
             @Override
             protected boolean matchesSafely(JsonObject item, Description mismatchDescription) {
-                return item.containsKey(property);
+                boolean matches = item.containsKey(property);
+                if (!matches) {
+                    mismatchDescription.appendText(
+                            "property " + "'" + property + "'" + " was not present. The following keys are present: " + item.fieldNames());
+                }
+                return matches;
             }
 
             @Override
             public void describeTo(Description description) {
-
+                description.appendText("a JsonObject with a property " + "'" + property + "'");
             }
         };
     }
+
 }
