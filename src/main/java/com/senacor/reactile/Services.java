3e99dbb1784f3a304c6990f1e43f5f84c7bcc56b
Martin Blume <martin.blume@senacor.com>
2015-06-25 12:20:51 +0200

    Added BranchService to Services

--- src/main/java/com/senacor/reactile/Services.java
+++ src/main/java/com/senacor/reactile/Services.java
@@ -1,8 +1,8 @@
 package com.senacor.reactile;
 
-import com.google.common.collect.ImmutableSet;
-
 import java.util.Set;
+
+import com.google.common.collect.ImmutableSet;
 
 public enum Services implements ServiceIdProvider {
 
@@ -11,13 +11,14 @@ public enum Services implements ServiceIdProvider {
     UserService("com.senacor.innolab.reactile.user-service", UserConnector),
     CustomerService("com.senacor.innolab.reactile.customer-service", EmbeddedMongo),
     AppointmentSerivce("com.senacor.innolab.reactile.appointment-service"),
+    BranchService("com.senacor.innolab.reactile.branch-service"),
     AccountService("com.senacor.innolab.reactile.account-service", EmbeddedMongo),
     CreditCardService("com.senacor.innolab.reactile.creditcard-service", EmbeddedMongo),
     TransactionService("com.senacor.innolab.reactile.transaction-service", EmbeddedMongo),
     PushNotificationService("com.senacor.innolab.reactile.pushnotification-service"),
     HystrixMetricsStreamVerticle("com.senacor.innolab.reactile.hystrix-metrics-stream-service"),
-    GatewayService("com.senacor.innolab.reactile.gateway-service", UserService, CustomerService, AccountService, CreditCardService, TransactionService, PushNotificationService, HystrixMetricsStreamVerticle);
-
+    GatewayService("com.senacor.innolab.reactile.gateway-service", UserService, CustomerService, AccountService, CreditCardService,
+        TransactionService, PushNotificationService, HystrixMetricsStreamVerticle);
 
     private final String serviceName;
     private final Set<ServiceIdProvider> dependencies;
@@ -41,6 +42,5 @@ public enum Services implements ServiceIdProvider {
     public int order() {
         return ordinal();
     }
-
 
 }
