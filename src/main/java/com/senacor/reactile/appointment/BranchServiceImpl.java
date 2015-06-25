ecad14b12724eea56ebfb1b0c26baabe0a6310dc
Martin Blume <martin.blume@senacor.com>
2015-06-25 12:25:39 +0200

    Started implementing BranchService

--- src/main/java/com/senacor/reactile/appointment/BranchServiceImpl.java
+++ src/main/java/com/senacor/reactile/appointment/BranchServiceImpl.java
@@ -1,12 +1,27 @@
 package com.senacor.reactile.appointment;
 
+import com.google.inject.Inject;
+import com.senacor.reactile.rx.Rx;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
+import rx.Observable;
 
 public class BranchServiceImpl implements BranchService {
 
+    private final BranchDatabase branchDatabase;
+
+    @Inject
+    public BranchServiceImpl(BranchDatabase branchDatabase) {
+        this.branchDatabase = branchDatabase;
+    }
+
     @Override
     public void getBranch(String branchId, Handler<AsyncResult<Branch>> resultHandler) {
-        return;
+        Rx.bridgeHandler(getBranch(branchId), resultHandler);
+    }
+
+    public Observable<Branch> getBranch(String branchId) {
+        // TODO
+        return null;
     }
 }
