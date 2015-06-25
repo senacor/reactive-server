fdb5ea8948c2a88be33f0c403d676f59f16bbdfa
Michael Menzel <michael.menzel@senacor.com>
2015-06-25 12:28:12 +0200

    Dokumentation CustomerService Interface

--- src/main/java/com/senacor/reactile/customer/CustomerService.java
+++ src/main/java/com/senacor/reactile/customer/CustomerService.java
@@ -5,6 +5,19 @@ import io.vertx.codegen.annotations.VertxGen;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 
+
+/* Interface für den Customerservice
+
+@ProxyGen: generiert die Klassen CustomerserviceVertxEBProxy und CustomerServiceVertxProxyHandler in build\generated-src\...\customer\
+CustomerserviceVertxEBProxy: Client
+CustomerServiceVertxProxyHandler: Handler zur Registrierung im Verticle. Mappt Nachrichten auf die Methoden einer CostomerService-Instanz
+CustomerserviceVertxEBProxy: Client-Implementierung des CustomerService-Interfaces. Parameter der Methodenaufrufe werden nach JSON konvertiert, die Aktion gesetzt und dann versand.
+
+@VertxGen: generiert die Klasse CustomerService in build\generated-src\...rxjava\customer\
+Die Klasse dekoriert eine Implementierung von CustomerService um Observable-Methoden
+*/
+
+
 @ProxyGen
 @VertxGen
 public interface CustomerService {
