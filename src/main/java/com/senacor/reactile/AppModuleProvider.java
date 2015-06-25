e64a9f50e09057806f7134ec3d42ca38bca6a9a9
Martin Blume <martin.blume@senacor.com>
2015-06-25 12:13:17 +0200

    Added BranchService to AppModuleProvider

--- src/main/java/com/senacor/reactile/AppModuleProvider.java
+++ src/main/java/com/senacor/reactile/AppModuleProvider.java
@@ -1,12 +1,19 @@
 package com.senacor.reactile;
 
+import static com.google.inject.matcher.Matchers.annotatedWith;
+import static com.google.inject.matcher.Matchers.any;
+
 import com.deenterprised.vertx.spi.BootstrapModuleProvider;
-import com.google.inject.*;
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.senacor.reactile.account.AccountService;
 import com.senacor.reactile.account.AccountServiceImpl;
 import com.senacor.reactile.account.TransactionService;
 import com.senacor.reactile.account.TransactionServiceImpl;
+import com.senacor.reactile.appointment.BranchService;
+import com.senacor.reactile.appointment.BranchServiceImpl;
 import com.senacor.reactile.creditcard.CreditCardService;
 import com.senacor.reactile.creditcard.CreditCardServiceImpl;
 import com.senacor.reactile.customer.CustomerService;
@@ -30,9 +37,6 @@ import io.vertx.rx.java.RxHelper;
 import io.vertx.serviceproxy.ProxyHelper;
 import rx.Scheduler;
 
-import static com.google.inject.matcher.Matchers.annotatedWith;
-import static com.google.inject.matcher.Matchers.any;
-
 public class AppModuleProvider implements BootstrapModuleProvider {
     @Override
     public Module get() {
@@ -54,17 +58,14 @@ public class AppModuleProvider implements BootstrapModuleProvider {
             bind(TransactionService.class).to(TransactionServiceImpl.class);
             bind(CustomerService.class).annotatedWith(Impl.class).to(CustomerServiceImpl.class);
             bind(UserConnector.class);
+            bind(BranchService.class).annotatedWith(Impl.class).to(BranchServiceImpl.class);
 
             // Install  HystrixComand Factories
-            install(new FactoryModuleBuilder()
-                    .implement(CustomerUpdateAddressCommand.class, CustomerUpdateAddressCommand.class)
-                    .build(CustomerUpdateAddressCommandFactory.class));
-            install(new FactoryModuleBuilder()
-                    .implement(StartCommand.class, StartCommand.class)
-                    .build(StartCommandFactory.class));
-            install(new FactoryModuleBuilder()
-                    .implement(CustomerServiceImplUpdateAddressCommand.class, CustomerServiceImplUpdateAddressCommand.class)
-                    .build(CustomerServiceImplUpdateAddressCommandFactory.class));
+            install(new FactoryModuleBuilder().implement(CustomerUpdateAddressCommand.class, CustomerUpdateAddressCommand.class).build(
+                CustomerUpdateAddressCommandFactory.class));
+            install(new FactoryModuleBuilder().implement(StartCommand.class, StartCommand.class).build(StartCommandFactory.class));
+            install(new FactoryModuleBuilder().implement(CustomerServiceImplUpdateAddressCommand.class, CustomerServiceImplUpdateAddressCommand.class)
+                .build(CustomerServiceImplUpdateAddressCommandFactory.class));
 
             HystrixCommandInterceptor hystrixCommandInterceptor = new HystrixCommandInterceptor();
             requestInjection(hystrixCommandInterceptor);
@@ -107,6 +108,11 @@ public class AppModuleProvider implements BootstrapModuleProvider {
         com.senacor.reactile.rxjava.account.AccountService provideAccountService(Vertx vertx) {
             AccountService proxy = ProxyHelper.createProxy(AccountService.class, vertx, AccountService.ADDRESS);
             return new com.senacor.reactile.rxjava.account.AccountService(proxy);
+        }
+
+        @Provides
+        BranchService provideBranchService(Vertx vertx) {
+            return ProxyHelper.createProxy(BranchService.class, vertx, BranchService.ADDRESS);
         }
     }
 }
