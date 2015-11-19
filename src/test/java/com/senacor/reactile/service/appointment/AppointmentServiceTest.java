6b48a33bd4a658dc7c33be859a1596901c88f949
Andreas Karoly <andreas.karoly@senacor.com>
2015-11-19 13:21:43 +0100

    refactor tests

--- src/test/java/com/senacor/reactile/service/appointment/AppointmentServiceTest.java
+++ src/test/java/com/senacor/reactile/service/appointment/AppointmentServiceTest.java
@@ -2,38 +2,29 @@ package com.senacor.reactile.service.appointment;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
 
-import javax.inject.Inject;
+import com.google.common.base.VerifyException;
 
-import com.senacor.reactile.Services;
-import com.senacor.reactile.VertxRule;
-import com.senacor.reactile.guice.GuiceRule;
-
-import org.junit.ClassRule;
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
-
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.impl.LoggerFactory;
 
 /**
  * @author Andreas Karoly, Senacor Technologies AG
  */
 public class AppointmentServiceTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(AppointmentServiceTest.class);
-
-    @ClassRule
-    public final static VertxRule vertxRule = new VertxRule(Services.AppointmentService);
-
-    @Rule
-    public final GuiceRule guiceRule = new GuiceRule(vertxRule.vertx(), this);
-
-    @Inject
     private AppointmentService service;
+    private AppointmentDatabase appointmentDatabase;
 
+    @Before
+    public void setUp() {
+        appointmentDatabase = new AppointmentDatabase();
+        service = new AppointmentServiceImpl(appointmentDatabase);
+    }
 
     @Test
     public void shouldReturnAppointments() {
@@ -41,4 +32,40 @@ public class AppointmentServiceTest {
         assertThat(appointments.getAppointmentList(), is(not(empty())));
     }
 
+    @Test
+    public void shouldReturnSpecificAppointmentById() {
+        Appointment appointment = initializeAppointment();
+
+        Appointment fetchedAppointment = service.getAppointmentById(appointment.getId()).toBlocking().first();
+
+        assertThat(fetchedAppointment, is(notNullValue()));
+        assertThat(fetchedAppointment.getId(), is(appointment.getId()));
+    }
+
+    @Test(expected = VerifyException.class)
+    public void shouldThrowExceptionOnInvalidAppointmentId() {
+        service.getAppointmentById(null);
+    }
+
+    @Test
+    public void shouldReturnAppointmentsByCustomerId() {
+        Appointment appointment = initializeAppointment();
+
+        AppointmentList fetchedAppointments = service.getAppointmentsByCustomer("cust-10042").toBlocking().first();
+
+        assertThat(fetchedAppointments.getAppointmentList(), hasSize(1));
+        assertThat(fetchedAppointments.getAppointmentList().get(0).getId(), is(appointment.getId()));
+    }
+
+    @Test(expected = VerifyException.class)
+    public void shouldThrowExceptionOnInvalidCustomerId() {
+        service.getAppointmentsByCustomer(null);
+    }
+
+    private Appointment initializeAppointment() {
+        Appointment appointment = Appointment.newBuilder().withId("42").withCustomerId("cust-10042").build();
+        appointmentDatabase.saveOrUpdate(appointment);
+
+        return appointment;
+    }
 }
