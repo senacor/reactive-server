95e3436c28fc8e45ee872d21b5d77cb3883cfcd5
Janusz Tymoszuk <janusz.tymoszuk@senacor.com>
2015-11-19 17:02:07 +0100

    News stub

---
+++ src/main/java/com/senacor/reactile/service/newsticker/NewsServiceImpl.java
@@ -0,0 +1,13 @@
+package com.senacor.reactile.service.newsticker;
+
+import rx.Observable;
+
+public class NewsServiceImpl implements NewsService{
+
+	@Override
+	public Observable<NewsCollection> getLatestNews(int max) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
