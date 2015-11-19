fcea24491e6e30fa04b4a097d7d321a34a9e94ba
Thomas Kreyling <Thomas.Kreyling@googlemail.com>
2015-11-19 14:50:59 +0100

    implemented findBranches

--- src/main/java/com/senacor/reactile/service/branch/BranchServiceImpl.java
+++ src/main/java/com/senacor/reactile/service/branch/BranchServiceImpl.java
@@ -3,17 +3,23 @@ package com.senacor.reactile.service.branch;
 import com.senacor.reactile.json.JsonizableList;
 import rx.Observable;
 
+import java.util.List;
+
 public class BranchServiceImpl implements BranchService {
     private final BranchDatabase branchDatabase = new BranchDatabase();
 
     @Override
     public Observable<Branch> getBranch(String branchId) {
-        return Observable.just(branchDatabase.findById(branchId));
+        return Observable.defer(()-> Observable.just(branchDatabase.findById(branchId)));
     }
 
     @Override
     public Observable<BranchList> findBranches(JsonizableList<String> branchIds) {
-        return null;
+        return Observable.defer(()-> {
+            List<Branch> branches = branchDatabase.findByIds(branchIds.toList());
+            BranchList branchList = BranchList.newBuilder().withBranches(branches).build();
+            return Observable.just(branchList);
+        });
     }
 
     @Override
--- src/test/java/com/senacor/reactile/service/branch/BranchServiceTest.java
+++ src/test/java/com/senacor/reactile/service/branch/BranchServiceTest.java
@@ -3,6 +3,7 @@ package com.senacor.reactile.service.branch;
 import com.senacor.reactile.Services;
 import com.senacor.reactile.VertxRule;
 import com.senacor.reactile.guice.GuiceRule;
+import com.senacor.reactile.json.JsonizableList;
 import com.senacor.reactile.mongo.MongoInitializer;
 import com.senacor.reactile.service.customer.Customer;
 import com.senacor.reactile.service.customer.CustomerFixtures;
@@ -15,6 +16,10 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import javax.inject.Inject;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.senacor.reactile.domain.IdentityMatchers.hasId;
 import static org.junit.Assert.assertThat;
@@ -34,5 +39,16 @@ public class BranchServiceTest {
     public void thatBranchIsReturned() {
         Branch branch = service.getBranch("1").toBlocking().first();
         assertThat(branch.getId(), CoreMatchers.equalTo("1"));
+    }
+
+    @Test
+    public void thatBranchesAreReturned() {
+        JsonizableList<String> branchIds = new JsonizableList<>(Arrays.asList("1", "2"));
+
+        BranchList branchList = service.findBranches(branchIds).toBlocking().first();
+
+        List<String> returnedBranchIds = branchList.getBranches().stream().map(Branch::getId).collect(Collectors.toList());
+
+        assertThat(returnedBranchIds, CoreMatchers.equalTo(Arrays.asList("1", "2")));
     }
 }
